cf55e4c829cbbedbc024507f977b6c41cc78e56f
TJonCanon <4923@holbertonschool.com>
2022-05-23 12:08:10 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 97;
+
+	while (ch <= 122)
+	{
+	if (ch != 113 && ch != 101)
+		putchar(ch);
+		ch++;
+	}
+	putchar ('\n');
+	return (0);
+}
