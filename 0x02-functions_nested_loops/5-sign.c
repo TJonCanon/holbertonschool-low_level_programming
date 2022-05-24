6073b85d64b48823dd621659c6d5df94b5513732
TJonCanon <4923@holbertonschool.com>
2022-05-24 11:49:26 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * @n: yuh
+ *
+ * Return: Always 0 (Success)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+{
+	_putchar (43);
+	return (1);
+}
+	else if (n == 0)
+{
+	_putchar (48);
+	return (0);
+}
+	else
+{
+	_putchar (95);
+	return (-1);
+}
+}
