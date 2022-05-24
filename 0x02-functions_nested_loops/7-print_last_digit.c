a4c98cd6194269d16e0250bf632b3b522a859cc7
TJonCanon <4923@holbertonschool.com>
2022-05-24 13:13:29 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: blah
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	_putchar(n + '0');
+	return (n);
+}
