91c4a40e07f8f72d34313e66854bb8877d4377e2
TJonCanon <4923@holbertonschool.com>
2022-05-31 09:00:52 -0700

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * print_most_numbers - blah
+ *
+ * return : Always (0)
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+	if (x != 50 && x != 52)
+	
+	{
+		_putchar(x);
+	}
+	}
+	_putchar ('\n');
+}
