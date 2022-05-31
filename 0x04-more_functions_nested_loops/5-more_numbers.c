76af6e960002aa9369d72325046b1236b0fe8c01
TJonCanon <4923@holbertonschool.com>
2022-05-31 09:35:05 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - blah
+ *
+ * Return - always (0)
+ *
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		for (y= 0 ; y <= 14 ; y++)
+	{
+		if (y >= 10)
+		{
+		_putchar ('0' + (y / 10));
+		}
+		_putchar('0' + (y % 10));
+	}
+	_putchar ('\n');
+
+	}
+}
