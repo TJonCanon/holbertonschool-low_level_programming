3a9fb1ef893fb541ee2543ce7a6ae50e4978dbfd
TJonCanon <4923@holbertonschool.com>
2022-05-30 19:36:15 -0700

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,10 +24,15 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
-	else
+	else if (a == b && a > c)
 	{
 		largest = a;
 	}
+	else if (a == c && a > b)
+	{
+		largest = a;
+	} else
+		largest = b;
 
-    return (largest);
+	return (largest);
 }
