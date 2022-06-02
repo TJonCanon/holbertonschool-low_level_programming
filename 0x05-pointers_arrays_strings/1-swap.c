bb232c8d59c061254ef51e96554fa96142b0efb0
TJonCanon <4923@holbertonschool.com>
2022-06-02 16:27:15 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of two ints
+ *
+ * @a: swapped variable 1
+ * @b: swapped variable 2
+ *
+ * Return 0
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
