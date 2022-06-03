7dc28c0ef6b4a4850981098bfcdbb5840bed7df2
TJonCanon <4923@holbertonschool.com>
2022-06-02 17:20:43 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ *
+ * @s: character
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	return (i);
+}
