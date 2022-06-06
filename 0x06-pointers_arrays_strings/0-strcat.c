ad97662135fe1713c392594e3bb2ac558cd2c55d
TJonCanon <4923@holbertonschool.com>
2022-06-06 14:04:15 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * *strcat - concatenates two strings
+ *
+ * @dest: string
+ *
+ * @src: string
+ *
+ * Return: pointer dest
+ */
+char *strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+return (dest);
+}
