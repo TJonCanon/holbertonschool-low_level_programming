477e0436bb207f9fc2697381503635a04a0330d5
TJonCanon <4923@holbertonschool.com>
2022-06-06 18:04:03 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters to uppercase
+ *
+ * @s: capitalized letters
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = (s[i] - 32);
+		}
+		i++
+	}
+	return (s);
+}
