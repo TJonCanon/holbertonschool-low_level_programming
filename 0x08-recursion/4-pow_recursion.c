a3c5f851d4580dbab03d1743bb62bc12ae298cfa
TJonCanon <4923@holbertonschool.com>
2022-06-08 18:02:32 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that recursively multiplies x by itself y times
+ * 
+ * @x: multiplied number
+ *
+ * @y: number of times to make n go up
+ * 
+ * Return: I am confused 
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
