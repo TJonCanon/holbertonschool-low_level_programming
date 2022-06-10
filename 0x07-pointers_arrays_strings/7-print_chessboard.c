ccb5fb2a47005340975641155787a290a7b35f72
TJonCanon <4923@holbertonschool.com>
2022-06-09 17:11:19 -0700

    task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,7 +4,19 @@
  *
  * @a: array
  *
- * Return - chessboard
+ * Return: chessboard
  */
 void print_chessboard(char (*a)[8])
 {
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+
+		_putchar(10);
+	}
+}
