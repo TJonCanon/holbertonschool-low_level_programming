d850adf48f19c530a5d4b7618607b1ceeb341d7d
TJonCanon <4923@holbertonschool.com>
2022-06-14 16:28:49 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds numbers
+ * @argv: vector
+ * @argc: counter
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i, n, sum;
+
+	for (n = 1; n < argc; n++)
+	{
+		for (i = 0; argv[n][i]; i++)
+		{
+			if (argv[n][i] < '0' || argv[n][i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[n]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
