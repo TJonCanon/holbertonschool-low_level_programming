13d87920c43d19fac9eed9a66f42ccc01bdbfef7
TJonCanon <4923@holbertonschool.com>
2022-06-14 10:22:17 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Program that prints all arguments it receives
+ * @argc: count
+ * @argv: vector
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	int i;
-	for (i=0; i<argc; i++)
+
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	return 0;
+	return (0);
 }
