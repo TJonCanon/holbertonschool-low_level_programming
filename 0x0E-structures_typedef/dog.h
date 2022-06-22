cd3dc6b70f2783814018c6599c952a132a2a7fba
TJonCanon <4923@holbertonschool.com>
2022-06-22 13:48:33 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,8 @@
 
 #ifndef DOG_H
 #define DOG_H
-
 #include <stdio.h>
 #include <stdlib.h>
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
  * struct dog - defines new struct
@@ -21,8 +18,14 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
