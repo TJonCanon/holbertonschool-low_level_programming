6efe914a8951f7fcd28cad56899de9119e21514c
TJonCanon <4923@holbertonschool.com>
2022-06-23 14:43:48 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that searches for an integer
+ *
+ * @array: array
+ *
+ * @size: size
+ *
+ * @cmp: cmp
+ *
+ * Return: NULL
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+	return (i);
+		}
+	}
+	return (-1);
+}
