01b93878a82cc0bb87601b2f150e1debc32c3906
TJonCanon <4923@holbertonschool.com>
2022-07-14 21:10:10 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (node);
 	}
 
-	for (c = 0; c < idx && temp; c++)
+	for (c = 0; c < idx && data; c++)
 	{
 		if (c == (idx - 1))
 		{
