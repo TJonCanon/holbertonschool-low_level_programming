9b571418f930bbbff599aae6f5daf96c9f9bc10c
TJonCanon <4923@holbertonschool.com>
2022-07-19 14:31:46 -0700

    main.h

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <uinstd.h>
+#include <fcnt1.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+
+#endif /* MAIN_H */
