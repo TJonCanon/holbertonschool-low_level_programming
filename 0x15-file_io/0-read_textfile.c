91b980737ecce9aac7c9888d130821082e64d1ad
TJonCanon <4923@holbertonschool.com>
2022-07-19 19:01:12 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	int readit;
+	ssize_t length;
+
+	if (filename == NULL)
+		return (0);
+	readit = open(filename, O_RDONLY);
+	if (readit < 0)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(readit);
+		return (0);
+	}
+	length = read(readit, buffer, letters);
+	close (readit);
+	if (length < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	length = write(STDOUT_FILENO, buffer, length);
+	if (length < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	return (length);
+}
