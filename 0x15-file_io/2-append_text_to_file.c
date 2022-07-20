bc5ca230ac639b02e5a2fcf3749115218085a4d0
TJonCanon <4923@holbertonschool.com>
2022-07-19 19:25:16 -0700

    task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to a file
+ *
+ * @file: name of the file appended
+ *
+ * @text_content: contents to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int append;
+
+	if (filename == NULL)
+		return (-1);
+	append = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (append < 0)
+		return (-1);
+	if (text_content != NULL)
+	{
+		write(append, text_content, strlen(text_content));
+	}
+	close(append);
+	return (1);
+}
