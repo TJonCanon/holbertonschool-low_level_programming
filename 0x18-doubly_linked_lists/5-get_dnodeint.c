64e7ef533a664013540d8201d5031ca3bd7e4d44
TJonCanon <4923@holbertonschool.com>
2022-08-02 21:25:31 -0700

    task 5

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	while (head != NULL)
 	{
-		if (l == NULL)
+		if (l == index)
 			return (head);
 		l++;
 		head = head->next;
