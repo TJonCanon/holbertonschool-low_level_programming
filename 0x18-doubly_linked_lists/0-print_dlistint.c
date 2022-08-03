d68e352ddb822d928b4260d39a2f84a0f9933187
TJonCanon <4923@holbertonschool.com>
2022-08-02 17:47:54 -0700

    task 1

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+/**
+ * print_dlistint - prints the elements of a list
+ *
+ * @h: list
+ *
+ * Return: node number
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int l = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		++l;
+		h = h->next;
+	}
+	return(l);
+}
