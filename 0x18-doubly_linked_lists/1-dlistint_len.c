115eff8552b6c225dc83a5de64fb2dd76aef81cc
TJonCanon <4923@holbertonschool.com>
2022-08-02 19:02:31 -0700

    task 1

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - returns amount of elements in list
+ *
+ * @h: list
+ *
+ * Return: number of elements in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int l = 0;
+
+	while (h != NULL)
+	{
+		l++;
+		h = h-next;
+	}
+	return (l);
+}
