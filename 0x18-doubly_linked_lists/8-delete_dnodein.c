e7aa88fd176b54f2c6f79c25ca378f12810e1051
TJonCanon <4923@holbertonschool.com>
2022-08-02 21:34:19 -0700

    task 8 kind of

---
+++ 0x18-doubly_linked_lists/8-delete_dnodein.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+* delete_dnodeint_at_index - it supposed to print all the elements of a list backwards
+* @head: head
+* @index: index of the node that should be deleted
+* Nathan found this bug. It is kind of great. I do not claim this one. 
+* Return: 1 if succeeded, -1 if failed
+*/
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	(void)head;
+	(void)index;
+
+	return (-1);
+}
