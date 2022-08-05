2a0afbf6a731e50cba4eafce52d3dfe2b032f1cd
TJonCanon <4923@holbertonschool.com>
2022-08-04 20:40:09 -0700

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size
+ *
+ * Return: hash
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int count;
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
+
+	if (!hash)
+		return (NULL);
+
+	hash->array = malloc(sizeof(hash_table_t *) * size);
+
+	if (!hash->array)
+		return (NULL);
+
+	hash->size = size;
+
+	for (count = 0; count < size; count++)
+		hash->array[i] = NULL;
+
+	return (hash);
+}
